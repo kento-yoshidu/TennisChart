cb28380f23a6e1cd1408c0c755d8712b7b1de3ad
kento <kento@toriwatari.bird>
2022-04-07 20:20:57 +0900

    ✨ #16 画像と本文表示

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,14 +1,43 @@
+import React from "react"
+import Image, { ImageProps } from "next/image"
 import { client } from "../../lib/client"
 
+type IProps = Omit<ImageProps, 'src' | 'width' | 'height' | 'layout'> & {
+  src: string
+  width: number
+  height: number
+  layout?: 'fixed' | 'intrinsic' | 'responsive'
+}
+
+interface Props {
+  params: {
+    id: string
+  }
+  locales?: string[]
+  locale: string
+  defaultLocale?: string
+}
+
+const MicroCMSImage: React.VFC<IProps> = ({ src, layout, alt, ...imageProps }) => (
+  <Image
+    {...imageProps}
+    src={src}
+    layout={layout ?? "responsive"}
+    placeholder="blur"
+    blurDataURL={`${src}?auto=compress&w=10`}
+    alt={alt}
+  />
+)
+
 export const getStaticPaths =async () => {
-  const data = await client.get({ endpoint: "blog" }) 
+  const data = await client.get({ endpoint: "blog" })
 
   const paths = data.contents.map((content: Blog) => `/blog/${content.id}`)
 
   return { paths, fallback: false }
 }
 
-export const getStaticProps =async (content: any) => {
+export const getStaticProps = async (content: Props) => {
   const id = content.params.id
   const data = await client.get({ endpoint: "blog", contentId: id })
 
@@ -19,10 +48,17 @@ export const getStaticProps =async (content: any) => {
   }
 }
 
-const Blog = ({ blog }) => (
-  <>
-    <p>main </p>
-  </>
-)
+const Blog = ({ blog }: { blog: Blog }) => {
+  return (
+    <main>
+      <MicroCMSImage src={blog.image.url} width={blog.image.width} height={blog.image.height} />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`
+        }}
+      />
+    </main>
+  )
+}
 
 export default Blog
