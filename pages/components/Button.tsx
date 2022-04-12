2d02897e37abf056b4b49b977a7443746ad01847
kento <kento@toriwatari.bird>
2022-04-12 21:06:37 +0900

    👨‍⚕️ #16 Bugfix

--- pages/components/Button.tsx
+++ pages/components/Button.tsx
@@ -1,8 +1,9 @@
+import React from "react"
 import Link from "next/link"
 
 const Button = ({ path }: { path: string }) => (
   <button className="block mx-auto py-2 px-8 text-textGreen border-2 border-bgGreen-900 font-semibold rounded hover:bg-bgGreen-800">
-    <Link href={path}>
+    <Link href={path ?? ""}>
       HOME
     </Link>
   </button>
