c44229bd55c5fae937edf3acd7b6502b80d2a2ad
kento <kento@toriwatari.bird>
2022-04-17 19:33:39 +0900

    ✨ Update

--- pages/components/AboutItem.tsx
+++ pages/components/AboutItem.tsx
@@ -6,8 +6,8 @@ const AboutItem = (
   { path, title, text, width, height }:
   { path: string, title: string, text: string, width: number, height: number }
 ) => (
-  <div className="h-56 border text-center">
-    <div className="relative border">
+  <div className="h-96 text-center w-1/3">
+    <div className="relative mb-4">
       <Image
         src={path}
         width={width}
@@ -16,10 +16,10 @@ const AboutItem = (
       />
     </div>
 
-    <h3 className="text-textGreen">
+    <h3 className="mb-4 text-textGreen text-2xl font-bold">
       {title}
     </h3>
-    <p>
+    <p className="w-9/12 mx-auto tracking-widest">
       {text}
     </p>
   </div>
