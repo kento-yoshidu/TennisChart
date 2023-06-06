bea6ce715a5d0d911528b0083fe6ccd0fd4561d0
kento <kento@toriwatari.bird>
2023-06-06 20:43:23 +0900

    ⚡ Create Metadata

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,15 +1,22 @@
 import Head from "next/head"
 
+import { siteMetaData } from "../lib/constants"
+
 type Props = {
-  pageTitle: string
-  pageDesc: string
+  pageTitle?: string
+  pageDesc?: string
 }
 
 const Meta = ({ pageTitle, pageDesc }: Props) => {
+  const { siteTitle, siteDesc } = siteMetaData
+
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+  const desc =  pageDesc ?? siteDesc
+
   return (
     <Head>
-      <title>{pageTitle}</title>
-      <meta name="description" content={pageDesc} />
+      <title>{title}</title>
+      <meta name="description" content={desc} />
     </Head>
   )
 }
