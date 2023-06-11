f921764cfc8de972906157cd7c356c302ae828dd
kento <kento@toriwatari.bird>
2023-06-11 21:01:22 +0900

    ⚡ Update

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons"
 const Footer = () => (
   <footer className={styles.footer}>
     <a
-      href="https://github.com/kento-yoshidu/MyForms"
+      href="https://github.com/kento-yoshidu/TennisChart"
       className={styles.link}
       aria-label="GitHubリポジトリーへのリンク"
     >
--- data/data.ts
+++ data/data.ts
@@ -7,6 +7,7 @@ export type DataType1 = {
   "other"?: number
 }
 
+// GS優勝回数
 export const data1: DataType1[] = [
   {
     year: "2003",
@@ -125,7 +126,141 @@ export const data1: DataType1[] = [
   },
 ]
 
+// GS決勝進出回数
 export const data2: DataType1[] = [
+  {
+    year: "2004",
+    "Roger Federer": 3,
+    "other": 5
+  },
+  {
+    year: "2005",
+    "Roger Federer": 2,
+    "Rafael Nadal": 1,
+    "other": 5
+  },
+  {
+    year: "2006",
+    "Roger Federer": 4,
+    "Rafael Nadal": 2,
+    "other": 2
+  },
+  {
+    year: "2007",
+    "Roger Federer": 4,
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 1,
+    "other": 1
+  },
+  {
+    year: "2008",
+    "Roger Federer": 3,
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 1,
+    "Andy Murray": 1,
+    "other": 1
+  },
+  {
+    year: "2009",
+    "Roger Federer": 4,
+    "Rafael Nadal": 1,
+    "other": 3
+  },
+  {
+    year: "2010",
+    "Roger Federer": 1,
+    "Rafael Nadal": 3,
+    "Novak Dokovic": 1,
+    "Andy Murray": 1,
+    "other": 2
+  },
+  {
+    year: "2011",
+    "Roger Federer": 1,
+    "Rafael Nadal": 3,
+    "Novak Dokovic": 3,
+    "Andy Murray": 1
+  },
+  {
+    year: "2012",
+    "Roger Federer": 1,
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 3,
+    "Andy Murray": 2
+  },
+  {
+    year: "2013",
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 3,
+    "Andy Murray": 2,
+    "other": 1
+  },
+  {
+    year: "2014",
+    "Roger Federer": 1,
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 2,
+    "other": 3
+  },
+  {
+    year: "2015",
+    "Roger Federer": 2,
+    "Novak Dokovic": 3,
+    "Andy Murray": 1,
+    "other": 2
+  },
+  {
+    year: "2016",
+    "Novak Dokovic": 3,
+    "Andy Murray": 3,
+    "other": 2
+  },
+  {
+    year: "2017",
+    "Roger Federer": 2,
+    "Rafael Nadal": 3,
+    "other": 3
+  },
+  {
+    year: "2018",
+    "Roger Federer": 1,
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 2,
+    "other": 3
+  },
+  {
+    year: "2019",
+    "Roger Federer": 1,
+    "Rafael Nadal": 3,
+    "Novak Dokovic": 2,
+    "other": 2
+  },
+  {
+    year: "2020",
+    "Rafael Nadal": 1,
+    "Novak Dokovic": 2,
+    "other": 3
+  },
+  {
+    year: "2021",
+    "Novak Dokovic": 3,
+    "other": 5
+  },
+  {
+    year: "2022",
+    "Rafael Nadal": 2,
+    "Novak Dokovic": 1,
+    "other": 5
+  },
+  {
+    year: "2023",
+    "Novak Dokovic": 2,
+    "other": 2
+  }
+]
+
+// GSとMS優勝回数
+export const data3: DataType1[] = [
   {
     year: '2003',
     "Roger Federer": 2,
