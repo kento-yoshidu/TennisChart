6be1c991dfc6fae25fe6eb3352e09aa9e5e4edfe
kento <kento@toriwatari.bird>
2023-06-17 12:43:39 +0900

    ⚡ pUpdate Page2

--- pages/page2/index.tsx
+++ pages/page2/index.tsx
@@ -5,13 +5,18 @@ import Layout from "../../components/layout"
 import PageHeader from '../../components/page-header'
 import Meta from '../../components/meta'
 
+import styles from "../../styles/styles.module.css"
+import Loading from '../../components/loading'
+
 const Page2 = () => {
   const fetchData = async () => {
-    const res = await fetch("/api/api2")
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/gs2`)
+    // const res = await fetch(`http://localhost:8888/gs2`)
+
     return res.json()
   }
 
-  const { data, isLoading } = useQuery("data2", fetchData)
+  const { data, isLoading, error } = useQuery("data2", fetchData)
 
   return (
   <>
@@ -24,15 +29,20 @@ const Page2 = () => {
       <PageHeader
         title="2. GS決勝進出"
         post="2023年6月11日"
-        update="23023年6月11日"
+        update="23023年6月17日"
       />
 
-      {isLoading
-        ? (
-          <p>loading...</p>
-        ) : (
-          <Chart data={data.data2}/>
-        )}
+      <main className={styles.main}>
+
+        {error && <p>エラーが発生しました。</p>}
+
+        {isLoading
+          ? (
+            <Loading />
+          ) : (
+            <Chart data={data}/>
+          )}
+      </main>
     </Layout>
   </>
   )
